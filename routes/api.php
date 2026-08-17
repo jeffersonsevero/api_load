63a60e46504676db999e36c0130ccca316c3d61c
Jeff <jefferson.severo@refuturiza.com.br>
2026-08-17 09:54:18 -0300

    create routes

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('users', function () {
+
+    return User::query()->paginate(15);
+
+});
+
+Route::post('users', function (Request $request) {
+
+    $user = User::create($request->all());
+
+    return response()->json($user, 201);
+
+});
